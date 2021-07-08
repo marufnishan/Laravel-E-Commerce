45c45c5cde7a82e88e1f0e20d4a71069ae8ed5c2
Maruf Nishan <nasim15-2328@diu.edu.bd>
2021-07-08 14:56:27 +0600

    Mobile Responsive

--- resources/views/livewire/footer-component.blade.php
+++ resources/views/livewire/footer-component.blade.php
@@ -187,42 +187,27 @@
 							<div class="back-link-row">
 								<ul class="list-back-link" >
 									<li><span class="row-title">Mobiles:</span></li>
-									<li><a href="#" class="redirect-back-link" title="mobile">Mobiles</a></li>
-									<li><a href="#" class="redirect-back-link" title="yphones">YPhones</a></li>
-									<li><a href="#" class="redirect-back-link" title="Gianee Mobiles GL">Gianee Mobiles GL</a></li>
-									<li><a href="#" class="redirect-back-link" title="Mobiles Karbonn">Mobiles Karbonn</a></li>
-									<li><a href="#" class="redirect-back-link" title="Mobiles Viva">Mobiles Viva</a></li>
-									<li><a href="#" class="redirect-back-link" title="Mobiles Intex">Mobiles Intex</a></li>
-									<li><a href="#" class="redirect-back-link" title="Mobiles Micrumex">Mobiles Micrumex</a></li>
-									<li><a href="#" class="redirect-back-link" title="Mobiles Bsus">Mobiles Bsus</a></li>
-									<li><a href="#" class="redirect-back-link" title="Mobiles Samsyng">Mobiles Samsyng</a></li>
-									<li><a href="#" class="redirect-back-link" title="Mobiles Lenova">Mobiles Lenova</a></li>
+									<li><a href="#" class="redirect-back-link" title="mobile">Samsung</a></li>
+									<li><a href="#" class="redirect-back-link" title="yphones">Iphone</a></li>
+									<li><a href="#" class="redirect-back-link" title="Gianee Mobiles GL">One Plus</a></li>
+									<li><a href="#" class="redirect-back-link" title="Mobiles Karbonn">Walton</a></li>
+									
 								</ul>
 
 								<ul class="list-back-link" >
 									<li><span class="row-title">Tablets:</span></li>
-									<li><a href="#" class="redirect-back-link" title="Plesc YPads">Plesc YPads</a></li>
-									<li><a href="#" class="redirect-back-link" title="Samsyng Tablets" >Samsyng Tablets</a></li>
-									<li><a href="#" class="redirect-back-link" title="Qindows Tablets" >Qindows Tablets</a></li>
-									<li><a href="#" class="redirect-back-link" title="Calling Tablets" >Calling Tablets</a></li>
-									<li><a href="#" class="redirect-back-link" title="Micrumex Tablets" >Micrumex Tablets</a></li>
-									<li><a href="#" class="redirect-back-link" title="Lenova Tablets Bsus" >Lenova Tablets Bsus</a></li>
-									<li><a href="#" class="redirect-back-link" title="Tablets iBall" >Tablets iBall</a></li>
-									<li><a href="#" class="redirect-back-link" title="Tablets Swipe" >Tablets Swipe</a></li>
-									<li><a href="#" class="redirect-back-link" title="Tablets TVs, Audio" >Tablets TVs, Audio</a></li>
+									<li><a href="#" class="redirect-back-link" title="Plesc YPads">Samsung</a></li>
+									<li><a href="#" class="redirect-back-link" title="Samsyng Tablets" >Iphone</a></li>
+									<li><a href="#" class="redirect-back-link" title="Qindows Tablets" >One Plus</a></li>
+									<li><a href="#" class="redirect-back-link" title="Calling Tablets" >Walton</a></li>
 								</ul>
 
 								<ul class="list-back-link" >
 									<li><span class="row-title">Fashion:</span></li>
-									<li><a href="#" class="redirect-back-link" title="Sarees Silk" >Sarees Silk</a></li>
-									<li><a href="#" class="redirect-back-link" title="sarees Salwar" >sarees Salwar</a></li>
-									<li><a href="#" class="redirect-back-link" title="Suits Lehengas" >Suits Lehengas</a></li>
-									<li><a href="#" class="redirect-back-link" title="Biba Jewellery" >Biba Jewellery</a></li>
-									<li><a href="#" class="redirect-back-link" title="Rings Earrings" >Rings Earrings</a></li>
-									<li><a href="#" class="redirect-back-link" title="Diamond Rings" >Diamond Rings</a></li>
-									<li><a href="#" class="redirect-back-link" title="Loose Diamond Shoes" >Loose Diamond Shoes</a></li>
-									<li><a href="#" class="redirect-back-link" title="BootsMen Watches" >BootsMen Watches</a></li>
-									<li><a href="#" class="redirect-back-link" title="Women Watches" >Women Watches</a></li>
+									<li><a href="#" class="redirect-back-link" title="Sarees Silk" >Rich Man</a></li>
+									<li><a href="#" class="redirect-back-link" title="sarees Salwar" >Yellow</a></li>
+									<li><a href="#" class="redirect-back-link" title="Suits Lehengas" >Twelve</a></li>
+									<li><a href="#" class="redirect-back-link" title="Biba Jewellery" >Infinity</a></li>
 								</ul>
 
 							</div>
