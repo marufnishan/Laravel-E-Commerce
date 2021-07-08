5a9e77d8363d5f581371149491b0341fdd9bebc8
Maruf Nishan <nasim15-2328@diu.edu.bd>
2021-07-08 18:30:56 +0600

    Admin dashboard Added

--- resources/views/auth/reset-password.blade.php
+++ resources/views/auth/reset-password.blade.php
@@ -37,7 +37,7 @@
  --}}
 
 
- <x-guest-layout>
+ <x-base-layout>
     <main id="main" class="main-site left-sidebar">
    
    <div class="container">
@@ -83,4 +83,4 @@
    </div><!--end container-->
    
    </main>
-    </x-guest-layout>
+    </x-base-layout>
