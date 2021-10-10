e16e561260767443f53c5d1d459d904e204a8c33
Maruf Nishan <nasim15-2328@diu.edu.bd>
2021-10-10 22:41:25 +0600

    Add Search on Admin Product Page

--- app/Http/Livewire/Admin/AdminProductComponent.php
+++ app/Http/Livewire/Admin/AdminProductComponent.php
@@ -9,6 +9,7 @@ use Livewire\WithPagination;
 class AdminProductComponent extends Component
 {
     use WithPagination;
+    public $searchTerm;
 
     public function deleteProduct($id)
     {
@@ -33,7 +34,12 @@ class AdminProductComponent extends Component
 
     public function render()
     {
-        $products = Product::paginate(10);
+        $search = '%' . $this->searchTerm . '%';
+        $products = Product::where('name','LIKE',$search)
+        ->orwhere('stock_status','LIKE',$search)
+        ->orwhere('regular_price','LIKE',$search)
+        ->orwhere('sale_price','LIKE',$search)
+        ->orwhere('id','DESC',$search)->paginate(10);
         return view('livewire.admin.admin-product-component',['products'=>$products])->layout('layouts.base');
     }
 }
