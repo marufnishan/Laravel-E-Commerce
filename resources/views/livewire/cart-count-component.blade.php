170323b2cff92a071d28f49d7c37f4d6d3edb714
tusar27 <tusar15-2327@diu.edu.bd>
2022-01-31 22:55:34 +0600

    fotter problem fixed

--- resources/views/livewire/cart-count-component.blade.php
+++ resources/views/livewire/cart-count-component.blade.php
@@ -1,7 +1,7 @@
 
 <div class="wrap-icon-section minicart">
     <a href="{{route('product.cart')}}" class="link-direction">
-        <i class="fa fa-shopping-basket" aria-hidden="true"></i>
+        <i class="fa fa-shopping-cart" aria-hidden="true"></i>
         <div class="left-info">
         @if(Cart::instance('cart')->count() > 0)
             <span class="index">{{Cart::instance('cart')->count()}} items</span>
--- resources/views/livewire/wishlist-count-component.blade.php
+++ resources/views/livewire/wishlist-count-component.blade.php
@@ -1,6 +1,6 @@
 <div class="wrap-icon-section wishlist">
     <a href="{{route('product.wishlist')}}" class="link-direction">
-        <i class="fa fa-heart" aria-hidden="true"></i>
+        <i class="fa fa-shopping-bag" aria-hidden="true"></i>
         <div class="left-info">
             @if(Cart::instance('wishlist')->count() > 0)
             <span class="index">{{Cart::instance('wishlist')->count()}} items</span>
