053b941ec4ae26fbcdf9cba7c369988a53a13f9b
marufnishan <nasim15-2328@diu.edu.bd>
2022-06-09 18:03:31 +0600

    Seller Can Search Order By Order_Id Added

--- resources/views/livewire/seller/seller-order-component.blade.php
+++ resources/views/livewire/seller/seller-order-component.blade.php
@@ -12,13 +12,21 @@
             <div class="col-md-12">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        All Orders
-                        <a href="{{route('seller.allorderpdf')}}" style="margin-left: 5px;" class="btn btn-danger">Download PDF</a>
+                        <div class="row">
+                            <div class="col-md-4">
+                                All Orders
+                            </div>
+                            <div class="col-md-4">
+                                <a href="{{route('seller.allorderpdf')}}" style="margin-left: 5px;"
+                                class="btn btn-danger pull-right">Download PDF</a>
+                            </div>
+                            <div class="col-md-4">
+                                <input type="text" class="form-control" placeholder="Search...."
+                                    wire:model="searchTerm" />
+                            </div>
+                        </div>
                     </div>
                     <div class="panel-body">
-                        {{-- @if(Session::has('order_message'))
-                        <div class="alert alert-success" role="alert">{{Session::get('order_message')}}</div>
-                        @endif --}}
                         <table class="table table-striped">
                             <thead>
                                 <tr><th>Orders</th>
